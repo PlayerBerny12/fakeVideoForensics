e6cf693066c05e09627318da4e1afeff8d01e18e
PlayerBerny12 <ber.honza12@seznam.cz>
2023-04-30 11:04:58 +0200

    Integrating DFDF framework

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.6
 
-RUN apt update && apt upgrade -y && apt install -y cmake
+RUN apt update && apt upgrade -y && apt install -y cmake && rm -rf /var/lib/apt/lists/*
 WORKDIR /app
 COPY . .
 
-RUN pip install -r requirements.txt
+RUN python3 -m pip install -r requirements.txt --no-cache-dir  
 
-ENTRYPOINT ["bash"]
+CMD [ "uvicorn", "entrypoint:app", "--host", "0.0.0.0", "--port", "50500", "--reload"]
---
+++ entrypoint.py
@@ -0,0 +1,94 @@
+"""
+DeepfakeDetectionFramework
+--------------------------
+
+if any required method is written in python
+then import specific module and run the method
+directlly otherwise import subprocess module
+and run it as subprocess and parse the output
+
+e.g.:
+    from my_module import my_func1, my_func2
+    my_func1(x, y)
+    
+    -------------------------
+    
+    import subprocess
+    result = subprocess.run(["ls", "-l"], capture_output=True, text=True) 
+    
+    print(result.stdout)
+"""
+
+from fastapi import FastAPI
+from typing import Any
+
+import subprocess
+import os
+
+app = FastAPI()
+
+ID = 7
+
+def check_request(file_path: str):
+    """
+    Check file type if it is processable
+    """
+    return True
+
+def data_preparation(file_path: str):
+    """
+    Prepare data from request
+    
+    E.g.: convert to specific format (wav, mp3, ...) 
+    or split data to frames, etc.
+    """
+    
+    return file_path
+
+def detection_method(file_path: str):
+    """
+    Run detection method on prepared data    
+    """
+    process = subprocess.run(
+        ["python3", "-u", "main.py", "--output_path", "/tmp", "--fast",
+         "--video_path", "/app/obama.mp4"],
+        stdout=subprocess.PIPE,
+        encoding="utf-8"
+    )    
+    
+    # Remove generated output file
+    [ os.remove(f"/tmp/{f}") for f in os.listdir("/tmp") if f.endswith(".avi") ]
+    
+    try:
+        return float((process.stdout.splitlines()[-2]).split()[-1])            
+    except:
+        return None    
+    
+def results_normalization(results: Any):
+    """
+    Normalize data by converting to interval <0-1>
+    """
+    
+    return results
+
+@app.get("/detect")
+def detect(id: int, checksum: str, filename: str, status: int, type: int):
+    """
+    Entry point for receiving requests
+    """
+    file_path = f"/mnt/processingdata/{filename}"
+
+    if check_request(file_path):        
+        # Prepare data from request (optional)
+        file_path = data_preparation(file_path)
+
+        # Run detection method
+        results = detection_method(file_path)
+
+        # Convert results to interval <0-1> (optional)
+        if results:
+            results = results_normalization(results)
+
+        return {"RequestID": id, "Value": results, "MethodID": ID}
+    else:
+        return {"RequestID": id, "Value": None, "MethodID": ID}
